f3d7dea8b875e6b94c83ceba3a4a6348c32ad45c
neerajensritharan <neery1218@gmail.com>
2022-02-01 20:56:06 -0500

    part 2

--- d5/src/main.rs
+++ d5/src/main.rs
@@ -27,10 +27,12 @@ fn main() {
         })
         .collect();
 
-    // only keep horizontal and vertical lines
+    // only keep horizontal and vertical and diagonal lines
     let lines: Vec<(Point, Point)> = points
         .into_iter()
-        .filter(|(p1, p2)| p1.x == p2.x || p1.y == p2.y)
+        .filter(|(p1, p2)| {
+            p1.x == p2.x || p1.y == p2.y || (p1.x - p2.x).abs() == (p1.y - p2.y).abs()
+        })
         .collect();
 
     let mut seen: HashSet<Point> = HashSet::new();
@@ -42,25 +44,37 @@ fn main() {
                 let p = Point { x: p1.x, y };
                 if seen.contains(&p) {
                     seen_twice.insert(p);
-                }
-                else {
+                } else {
                     seen.insert(p);
                 }
             }
-        }
-        else if p1.y == p2.y {
+        } else if p1.y == p2.y {
             let (min_x, max_x) = (cmp::min(p1.x, p2.x), cmp::max(p1.x, p2.x));
-            for x in min_x..(max_x+1) {
+            for x in min_x..(max_x + 1) {
                 let p = Point { x, y: p1.y };
                 if seen.contains(&p) {
                     seen_twice.insert(p);
-                }
-                else {
+                } else {
                     seen.insert(p);
                 }
+            }
+        } else {
+            let step_x = if p2.x - p1.x > 0 { 1 } else { -1 };
+            let step_y = if p2.y - p1.y > 0 { 1 } else { -1 };
+            let mut i = 0;
+            while i <= (p2.x - p1.x).abs() {
+                let p = Point {
+                    x: p1.x + step_x * i,
+                    y: p1.y + step_y * i,
+                };
+                if seen.contains(&p) {
+                    seen_twice.insert(p);
+                } else {
+                    seen.insert(p);
+                }
+                i += 1;
             }
         }
     }
     println!("{}", seen_twice.len());
-
 }
