e7036891c5656b5210d02673e12bd95f347cfe7e
neerajensritharan <neery1218@gmail.com>
2022-02-07 23:22:26 -0500

    making d11 more rusty (heavily copied from timvisee)

--- d11/src/main.rs
+++ d11/src/main.rs
@@ -1,5 +1,13 @@
-use std::collections::HashSet;
-use std::collections::VecDeque;
+const NEIGHBORS: [(isize, isize); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
 
 fn print_grid(grid: &[Vec<u32>]) {
     for row in grid.iter() {
@@ -9,84 +17,44 @@ fn print_grid(grid: &[Vec<u32>]) {
 }
 
 fn main() {
-    let mut grid: Vec<Vec<u32>> = include_str!("input.txt")
+    let mut grid: Vec<Vec<u32>> = include_str!("test.txt")
         .lines()
         .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
-    let mut n_flashes = 0;
+    let row_size = grid.len();
+    let col_size = grid[0].len();
 
-    for step in 0..100 {
-        // increase energy level of all octopi
-        for i in 0..grid.len() {
-            for j in 0..grid[0].len() {
-                grid[i][j] += 1;
-            }
-        }
-
-        n_flashes += flash(&mut grid);
-
-        if grid.iter().all(|row| row.iter().all(|cell| *cell == 0)) {
-            println!("All flashed on step {}", step + 1);
-        }
-
-        print_grid(&grid);
-    }
-
+    let n_flashes = (0..100).fold(0, |acc, _| {
+        // increment
+        grid.iter_mut()
+            .for_each(|row| row.iter_mut().for_each(|cell| *cell += 1));
+        let l_flashes = (0..row_size)
+            .flat_map(|r| (0..col_size).map(move |c| (r, c)))
+            .fold(0, |acc, (r, c)| {
+                acc + (grid[r][c] > 9)
+                    .then(|| flash(&mut grid, r, c))
+                    .unwrap_or(0)
+            });
+        println!("{}", l_flashes);
+        acc + l_flashes
+    });
     println!("{}", n_flashes);
 }
 
-fn add(
-    i: i32,
-    j: i32,
-    grid: &mut [Vec<u32>],
-    flashed: &mut HashSet<(usize, usize)>,
-    q: &mut VecDeque<(usize, usize)>,
-) {
-    if i >= 0 && i < (grid.len() as i32) && j >= 0 && j < (grid[0].len() as i32) {
-        grid[i as usize][j as usize] += 1;
-        if grid[i as usize][j as usize] > 9 && !flashed.contains(&(i as usize, j as usize)) {
-            q.push_back((i as usize, j as usize));
-        }
-    }
-}
-
-fn flash(grid: &mut [Vec<u32>]) -> i32 {
-    let mut num_flashes = 0;
-    let mut flashed: HashSet<(usize, usize)> = HashSet::new();
-    let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
-
-    // initialize queue with flash-ready octopi
-    for i in 0..grid.len() {
-        for j in 0..grid[0].len() {
-            if grid[i][j] > 9 {
-                queue.push_back((i, j));
+fn flash(grid: &mut [Vec<u32>], i: usize, j: usize) -> i32 {
+    grid[i][j] = 0;
+    // all cells have been incremented, so there aren't any cells where cell.value == 0
+    // UNLESS it just flashed
+    NEIGHBORS
+        .map(|(d_y, d_x)| ((d_y + i as isize) as usize, (d_x + j as isize) as usize))
+        .into_iter()
+        .fold(1, |acc, (r, c)| {
+            match grid.get_mut(r).and_then(|row| row.get_mut(c)) {
+                Some(cell) if *cell > 0 => {
+                    *cell += 1;
+                    acc + (*cell > 9).then(|| flash(grid, r, c)).unwrap_or(0)
+                }
+                _ => acc,
             }
-        }
-    }
-
-    while let Some((i, j)) = queue.pop_front() {
-        if grid[i][j] <= 9 || flashed.contains(&(i, j)) {
-            continue;
-        }
-        flashed.insert((i, j));
-        num_flashes += 1;
-
-        let i_ = i as i32;
-        let j_ = j as i32;
-        add(i_ - 1, j_ - 1, grid, &mut flashed, &mut queue);
-        add(i_ - 1, j_, grid, &mut flashed, &mut queue);
-        add(i_ - 1, j_ + 1, grid, &mut flashed, &mut queue);
-        add(i_, j_ + 1, grid, &mut flashed, &mut queue);
-        add(i_, j_ - 1, grid, &mut flashed, &mut queue);
-        add(i_ + 1, j_ + 1, grid, &mut flashed, &mut queue);
-        add(i_ + 1, j_, grid, &mut flashed, &mut queue);
-        add(i_ + 1, j_ - 1, grid, &mut flashed, &mut queue);
-        grid[i][j] = 0;
-    }
-
-    for (i, j) in flashed {
-        grid[i][j] = 0;
-    }
-
-    num_flashes
+        })
 }
