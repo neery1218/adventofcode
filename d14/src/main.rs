5150db840db83464c7685455e7997a917291de5b
neerajensritharan <neery1218@gmail.com>
2022-02-13 23:11:47 -0500

    much more efficient implementation

--- d14/src/main.rs
+++ d14/src/main.rs
@@ -3,13 +3,21 @@ use std::{char, collections::HashMap};
 use itertools::*;
 
 fn main() {
-    let (template, insertion_rules): (&str, HashMap<(char, char), char>) =
-        include_str!("test.txt").lines().enumerate().fold(
-            ("", HashMap::new()),
+    let (template, insertion_rules): (HashMap<(char, char), i64>, HashMap<(char, char), char>) =
+        include_str!("input.txt").lines().enumerate().fold(
+            (HashMap::new(), HashMap::new()),
             |(mut template, mut rules), (i, line)| {
                 match i {
                     0 => {
-                        template = line;
+                        template = line
+                            .chars()
+                            .into_iter()
+                            .tuple_windows()
+                            .chain([(line.chars().last().unwrap(), ' ')].into_iter())
+                            .fold(HashMap::new(), |mut acc, (c1, c2)| {
+                                acc.entry((c1, c2)).and_modify(|e| *e += 1).or_insert(1);
+                                acc
+                            })
                     }
                     1 => {} // blank line
                     _ => {
@@ -25,39 +33,33 @@ fn main() {
             },
         );
 
-    println!(
-        "{} {} {:?}",
-        template,
-        insertion_rules.len(),
-        insertion_rules
-    );
     // abcd => ab, bc, cd
-    let process = (0..10).fold(String::from(template), |template, i| {
-        let iteration: String = template
-            .chars()
-            .tuple_windows()
-            .chain([(template.chars().last().unwrap(), ' ')].into_iter())
-            .flat_map(|(c1, c2)| match insertion_rules.get(&(c1, c2)) {
-                None => vec![c1],
-                Some(insert) => vec![c1, *insert],
+    let process = (0..40).fold(template, |template, _| {
+        template
+            .into_iter()
+            .fold(HashMap::new(), |mut acc, ((c1, c2), val)| {
+                match insertion_rules.get(&(c1, c2)) {
+                    None => {
+                        acc.entry((c1, c2)).and_modify(|e| *e += val).or_insert(val);
+                    }
+                    Some(inter_val) => {
+                        *acc.entry((c1, *inter_val)).or_insert(0) += val;
+                        *acc.entry((*inter_val, c2)).or_insert(0) += val;
+                    }
+                };
+                acc
             })
-            .collect();
-
-        println!("{}", i);
-        iteration
     });
 
-    let mut freq: Vec<(char, i32)> = process
-        .chars()
+    let mut freq: Vec<(char, i64)> = process
         .into_iter()
-        .fold(HashMap::new(), |mut acc, c| {
-            acc.entry(c).and_modify(|e| *e += 1).or_insert(1);
+        .fold(HashMap::new(), |mut acc, ((c1, _), val)| {
+            *acc.entry(c1).or_insert(0) += val;
             acc
         })
         .into_iter()
         .collect_vec();
 
     freq.sort_unstable_by_key(|k| k.1);
-    println!("{:?}", freq);
     println!("{}", freq.last().unwrap().1 - freq.first().unwrap().1);
 }
