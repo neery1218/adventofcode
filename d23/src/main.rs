a4701a55bc3233bddb4053d7a5036fe18147adf7
neerajensritharan <neery1218@gmail.com>
2022-03-07 10:44:38 -0500

    qd done in 30s!

--- d23/src/main.rs
+++ d23/src/main.rs
@@ -1,12 +1,12 @@
-use std::cmp;
-use std::collections::{BTreeMap, HashMap, HashSet};
+#![feature(let_chains)]
+
+use std::collections::{BTreeMap, HashMap};
 use std::fmt;
 
-const NEIGHBORS: [(isize, isize); 4] = [(-1, 0), (0, -1), (0, 1), (1, 0)];
-
 type Grid = BTreeMap<(isize, isize), char>;
-const N: usize = 2;
 
+const NEIGHBORS: [(isize, isize); 4] = [(-1, 0), (0, -1), (0, 1), (1, 0)];
+const MAX_ROOM_DEPTH: isize = 5;
 const DOORS: [(isize, isize); 4] = [(1, 3), (1, 5), (1, 7), (1, 9)];
 const ROOMS: [(isize, isize); 16] = [
     (2, 3),
@@ -29,56 +29,68 @@ const ROOMS: [(isize, isize); 16] = [
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 enum Pod {
-    A,
-    B,
-    C,
-    D,
+    A(usize),
+    B(usize),
+    C(usize),
+    D(usize),
 }
 
 impl Pod {
-    fn from(c: char) -> Self {
+    fn from(c: char, id: usize) -> Self {
         match c {
-            'A' => Pod::A,
-            'B' => Pod::B,
-            'C' => Pod::C,
-            'D' => Pod::D,
+            'A' => Pod::A(id),
+            'B' => Pod::B(id),
+            'C' => Pod::C(id),
+            'D' => Pod::D(id),
             _ => unreachable!("{}", c),
         }
     }
 
     fn energy_per_step(&self) -> usize {
         match self {
-            Pod::A => 1,
-            Pod::B => 10,
-            Pod::C => 100,
-            Pod::D => 1000,
+            Pod::A(_) => 1,
+            Pod::B(_) => 10,
+            Pod::C(_) => 100,
+            Pod::D(_) => 1000,
         }
     }
 
     fn in_room(&self, (r, c): (isize, isize)) -> bool {
         match &self {
-            Pod::A => [(2, 3), (3, 3)].contains(&(r, c)),
-            Pod::B => [(2, 5), (3, 5)].contains(&(r, c)),
-            Pod::C => [(2, 7), (3, 7)].contains(&(r, c)),
-            Pod::D => [(2, 9), (3, 9)].contains(&(r, c)),
+            Pod::A(_) => [(2, 3), (3, 3), (4, 3), (5, 3)].contains(&(r, c)),
+            Pod::B(_) => [(2, 5), (3, 5), (4, 5), (5, 5)].contains(&(r, c)),
+            Pod::C(_) => [(2, 7), (3, 7), (4, 7), (5, 7)].contains(&(r, c)),
+            Pod::D(_) => [(2, 9), (3, 9), (4, 9), (5, 9)].contains(&(r, c)),
         }
     }
 
-    // commented out bc it's easy to use incorrectly
-    // fn in_wrong_room(&self, rc: (isize, isize)) -> bool {
-    //     ROOMS.contains(&rc) && !self.in_room(rc)
-    // }
+    fn in_right_place(&self, (r, c): (isize, isize), gs: &GameState) -> bool {
+        if !self.in_room((r, c)) {
+            return false;
+        }
+
+        (r..(MAX_ROOM_DEPTH + 1)).all(|d| {
+            gs.pos
+                .get(&(d, c))
+                .map(|p| p.in_room((d, c)))
+                .unwrap_or(false)
+        })
+    }
+
+    fn in_wrong_room(&self, rc: (isize, isize)) -> bool {
+        ROOMS.contains(&rc) && !self.in_room(rc)
+    }
 
     fn in_door(&self, rc: (isize, isize)) -> bool {
         DOORS.contains(&rc)
     }
 
-    fn dist(&self, (r, c): (isize, isize)) -> usize {
+    fn dist(&self, (_, c): (isize, isize)) -> usize {
         match &self {
-            Pod::A => ((3 - c).abs() + cmp::min((2 - r).abs(), (3 - r).abs())) as usize,
-            Pod::B => ((5 - c).abs() + cmp::min((2 - r).abs(), (3 - r).abs())) as usize,
-            Pod::C => ((7 - c).abs() + cmp::min((2 - r).abs(), (3 - r).abs())) as usize,
-            Pod::D => ((9 - c).abs() + cmp::min((2 - r).abs(), (3 - r).abs())) as usize,
+            Pod::A(_) => (3 - c).abs() as usize,
+            Pod::B(_) => (5 - c).abs() as usize,
+            Pod::C(_) => (7 - c).abs() as usize,
+            Pod::D(_) => (9 - c).abs() as usize,
         }
     }
 }
@@ -86,10 +98,10 @@ impl Pod {
 impl fmt::Display for Pod {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Pod::A => write!(f, "A"),
-            Pod::B => write!(f, "B"),
-            Pod::C => write!(f, "C"),
-            Pod::D => write!(f, "D"),
+            Pod::A(id) => write!(f, "A{}", id),
+            Pod::B(id) => write!(f, "B{}", id),
+            Pod::C(id) => write!(f, "C{}", id),
+            Pod::D(id) => write!(f, "D{}", id),
         }
     }
 }
@@ -127,20 +139,23 @@ impl GameState {
     }
 
     /// sorted list of neighboring game states by manhattan distance
-    fn get_neighbor_states(&self, grid: &Grid) -> Vec<(GameState, usize)> {
+    fn get_neighbor_states(&self, grid: &Grid) -> Vec<(GameState, Pod, (isize, isize), usize)> {
         let mut states = BTreeMap::new();
-        states.insert(self.clone(), 0);
 
-        println!("{:?}", self.pos);
-        for rc in self.pos.keys() {
+        for (rc, pod) in &self.pos {
+            if pod.in_right_place(*rc, self) {
+                continue;
+            }
+
             GameState::get_moves_for_pod(self, *rc, grid, &mut states, 0);
         }
 
-        let mut vec_states: Vec<(GameState, usize)> = states
+        let mut vec_states: Vec<(GameState, Pod, (isize, isize), usize)> = states
             .into_iter()
             .filter(|(s, _)| s.pos.iter().all(|(rc, p)| !p.in_door(*rc)))
+            .map(|(s, (p, rc, e))| (s, p, rc, e))
             .collect();
-        vec_states.sort_unstable_by(|(gs_a, _), (gs_b, _)| gs_a.dist().cmp(&gs_b.dist()));
+        vec_states.sort_unstable_by(|(gs_a, ..), (gs_b, ..)| gs_a.dist().cmp(&gs_b.dist()));
         vec_states
     }
 
@@ -148,7 +163,7 @@ impl GameState {
         gs: &GameState,
         (r, c): (isize, isize),
         grid: &Grid,
-        states: &mut BTreeMap<GameState, usize>,
+        states: &mut BTreeMap<GameState, (Pod, (isize, isize), usize)>,
         energy: usize,
     ) {
         NEIGHBORS
@@ -168,11 +183,11 @@ impl GameState {
                     new_gs.pos.insert(rc, p);
 
                     match states.get(&new_gs) {
-                        Some(&e) if e <= energy + p.energy_per_step() => return,
+                        Some(&(.., e)) if e <= energy + p.energy_per_step() => return,
                         _ => (),
                     }
 
-                    states.insert(new_gs.clone(), energy + p.energy_per_step());
+                    states.insert(new_gs.clone(), (p, rc, energy + p.energy_per_step()));
                     GameState::get_moves_for_pod(
                         &new_gs,
                         rc,
@@ -192,11 +207,11 @@ fn print_grid(gs: &GameState, grid: &Grid) {
     }
     println!();
 
-    for i in 0..5 {
+    for i in 0..7 {
         for j in 0..13 {
             match gs.pos.get(&(i, j)) {
                 None => print!("{:2}|", grid.get(&(i as isize, j as isize)).unwrap()),
-                Some(s) => print!(" {}|", s),
+                Some(s) => print!("{}|", s),
             };
         }
         println!();
@@ -204,27 +219,28 @@ fn print_grid(gs: &GameState, grid: &Grid) {
 }
 
 fn main() {
-    let (grid, gs) = include_str!("test.txt").lines().enumerate().fold(
-        (BTreeMap::new(), GameState::new()),
-        |(mut grid, mut gs), (i, line)| {
+    let (grid, gs, _) = include_str!("test.txt").lines().enumerate().fold(
+        (BTreeMap::new(), GameState::new(), HashMap::new()),
+        |(mut grid, mut gs, mut count), (i, line)| {
             line.char_indices().for_each(|(j, c)| match c {
                 'A' | 'B' | 'C' | 'D' => {
-                    gs.pos.insert((i as isize, j as isize), Pod::from(c));
+                    let a = count.entry(c).and_modify(|e| *e += 1).or_insert(1);
+                    gs.pos.insert((i as isize, j as isize), Pod::from(c, *a));
                     grid.insert((i as isize, j as isize), '.');
                 }
                 c => {
                     grid.insert((i as isize, j as isize), c);
                 }
             });
-            (grid, gs)
+            (grid, gs, count)
         },
     );
 
     print_grid(&gs, &grid);
-
     let mut states = BTreeMap::new();
     let mut min_state = None;
-    search(&gs, 0, &grid, &mut min_state, &mut states);
+    let mut moves = HashMap::new();
+    search(&gs, 0, &grid, &mut min_state, &mut states, &mut moves);
     println!("{:?}", min_state);
 }
 
@@ -234,16 +250,22 @@ fn search(
     grid: &Grid,
     min_state: &mut Option<usize>,
     states: &mut BTreeMap<GameState, usize>,
+    moves: &mut HashMap<Pod, usize>,
 ) {
+    // print_grid(gs, grid);
     if gs.done() {
         match *min_state {
-            None => *min_state = Some(energy),
-            Some(e) if e > energy => *min_state = Some(energy),
+            Some(e) if e <= energy => return,
             _ => (),
         }
 
+        println!("Done! with energy {}", energy);
+        println!("{:?}", moves);
+        *min_state = Some(energy);
+
         return;
     }
+    // println!();
 
     // terminate early if solution has less energy than our current state
     match *min_state {
@@ -258,7 +280,29 @@ fn search(
     }
 
     states.insert(gs.clone(), energy);
-    for (neighbor, energy_used) in gs.get_neighbor_states(grid) {
-        search(&neighbor, energy + energy_used, grid, min_state, states);
+    for (neighbor, pod, rc, energy_used) in gs.get_neighbor_states(grid) {
+        if let Some(2) = moves.get(&pod) {
+            continue;
+        }
+        if let Some(1) = moves.get(&pod) && !pod.in_right_place(rc, &neighbor) {
+            continue
+        }
+        if pod.in_wrong_room(rc) {
+            continue;
+        }
+        if pod.in_room(rc) && !pod.in_right_place(rc, &neighbor) {
+            continue;
+        }
+
+        moves.entry(pod).and_modify(|e| *e += 1).or_insert(1);
+        search(
+            &neighbor,
+            energy + energy_used,
+            grid,
+            min_state,
+            states,
+            moves,
+        );
+        moves.entry(pod).and_modify(|e| *e -= 1);
     }
 }
